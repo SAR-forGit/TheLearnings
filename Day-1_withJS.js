d7112c652eba025119c5f2a49cca330b25a3da75
SAR-forGit <forwebtoons@protonmail.com>
2024-11-28 15:32:25 +0000

    .

--- Day-1_withJS.js
+++ Day-1_withJS.js
@@ -8,3 +8,9 @@ console.log ("Upper Case:", nigg.toUpperCase());
 
 // Slice
 console.log ("Slice some up:", nigg.slice(2,6)) //Does'nt go till 6 means will show up to 5 from 2 i.e (index, index-1)
+
+var sliced_nigg = nigg.slice(3,8)
+console.log ("Another way of slicing: ", sliced_nigg);
+
+// Length
+console.log ("Length of Variable Nigg:", nigg.length)
