4ca4a2512b7b70706eec917be024481b0ab67539
SAR-forGit <forwebtoons@protonmail.com>
2024-11-29 15:23:28 +0000

    .

---
+++ Day_2_withJS.js
@@ -0,0 +1,19 @@
+console.log ("Hi there Day-2")
+
+// Operators
+
+var num1=2, num2=5
+
+console.log ("Num1: ", num1, "\nNum2: ", num2)
+console.log ("addition:", num1 + num2)
+console.log ("subtraction:", num1 - num2)
+console.log ("multiplication:", num1 * num2)
+console.log ("division:", num1 / num2)
+console.log ("Remainder:", num1 % num2)
+
+// Comparision Operator
+
+var num1=69, num2="69"
+
+console.log (num1 == num2) //Output will be true 
+console.log (num1 === num2) //Output will be false beacause it will also check the datatype and here num1 is number and num2 is string
