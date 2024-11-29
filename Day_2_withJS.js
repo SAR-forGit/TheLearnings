e6ad554b27fb3b3cb739264777111d04939dec28
SAR-forGit <forwebtoons@protonmail.com>
2024-11-29 15:35:30 +0000

    .

--- Day_2_withJS.js
+++ Day_2_withJS.js
@@ -17,3 +17,25 @@ var num1=69, num2="69"
 
 console.log (num1 == num2) //Output will be true 
 console.log (num1 === num2) //Output will be false beacause it will also check the datatype and here num1 is number and num2 is string
+
+// not operator
+
+var bhool = 0 //eitehr write 'true' fully or 'false' fully or 1 or 0 
+console.log ("!bhool =", !bhool)
+
+// If-else
+
+age = 17
+
+if (age>18){
+    console.log ("You're eligible to vote!")
+}
+else {
+    console.log ('You are not eligible to vote!')
+}
+
+// Conditional Operator || Ternary operator
+
+age = 19
+
+age > 18 ? console.log ("You're eligible to vote!") : console.log ('You are not eligible to vote!')
