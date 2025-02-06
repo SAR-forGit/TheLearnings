2daf1fe0bacabe8dbb8caee3bebd235025efb860
SAR-forGit <forwebtoons@protonmail.com>
2025-02-06 07:06:44 +0000

    12:36PM Thu 6 Feb 2025

--- JavaScript Experience/Day_1_JS_ApnaCollege.js
+++ JavaScript Experience/Day_1_JS_ApnaCollege.js
@@ -1,3 +1,37 @@
+// Q: For a given array with marks of students -> [85, 97, 44, 37, 76, 60], Find the average marks of the entire class
+
+let marks = [85, 97, 44, 37, 76, 60];
+let total = 0;
+
+for (let mark of marks){
+    total = total + mark;
+}
+
+let average = total / marks.length;
+
+console.log(`Average marks of the entire class is: ${average}`);
+
+// Q: For a given array with price of 5 items -> [250, 645, 300, 900, 50], all items has an offer of 10% off on them. 
+// Change the array to store the final price after applying offer 
+
+let items = [250, 645, 300, 900, 50], i = 0;
+
+//way 1
+for (let val of items){
+    let offer = val / 10;
+    items[i] = items[i] - offer;
+    console.log(items[i]);
+    i++;
+}
+
+//way 2
+for(let i = 0; i<items.length; i++){
+    let offer = items[i] / 10;
+    items[i] -= offer;
+}
+
+console.log(items);
+
 //////////////////////////////////////////////// String Concatenation Apna College question ///////////////////////////////////////////////////////
 let givenName = prompt("Enter your name: ");
 
@@ -22,3 +56,5 @@ while(1){
     }
 }
 console.log(`Name = ${givenName}\nUser Name = ${userName}`);
+
+
