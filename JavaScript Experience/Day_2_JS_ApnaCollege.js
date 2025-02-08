c0732df5953c0797dc2bf461b890a49af94b96c9
SAR-forGit <forwebtoons@protonmail.com>
2025-02-08 13:31:52 +0000

    7:01 PM Sat, 08 Feb, 2025

--- JavaScript Experience/Day_2_JS_ApnaCollege.js
+++ JavaScript Experience/Day_2_JS_ApnaCollege.js
@@ -1,8 +1,24 @@
 // DOM - Document Object Manipulation
 
-let first_para = document.getElementById("ManId1");
-console.dir(first_para);
+//////////////////////////////////////////////////////// Get Element by: ///////////////////////////////////////////////////////////////////
+let first_para = document.getElementById("ManId");
+// console.dir(first_para);
 console.log(first_para);
 
 let second_para = document.getElementsByClassName("ManClass")
 console.log(second_para);
+
+let all_paras = document.getElementsByTagName("p");
+console.log(all_paras);
+
+//////////////////////////////////////////////////////// Query Selectors: //////////////////////////////////////////////////////////////////
+
+let first_para_qs = document.querySelector("#ManId")
+console.log(first_para_qs);
+
+let second_para_qs = document.querySelector(".ManClass")
+console.log(second_para_qs);
+
+let all_para_qs = document.querySelectorAll("p");
+console.log(all_para_qs)
+
