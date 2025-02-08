4a4303e00a4a5d7fe7ce2f3df85fb8cce636c86e
SAR-forGit <forwebtoons@protonmail.com>
2025-02-08 14:13:17 +0000

    7:42 PM Sat, 08 Feb 2025

--- JavaScript Experience/Day_2_JS_ApnaCollege.js
+++ JavaScript Experience/Day_2_JS_ApnaCollege.js
@@ -22,3 +22,28 @@ console.log(second_para_qs);
 let all_para_qs = document.querySelectorAll("p");
 console.log(all_para_qs)
 
+//////////////////////////////////////////////////////// Practice: /////////////////////////////////////////////////////////////////////
+
+// Q" Create a h2 heading element with text - "Hello JavaScript". Append from "from Apna College Students" to this text using JS 
+let hello_sayer = document.getElementById("Hello_js");
+console.log(hello_sayer);
+console.log(typeof hello_sayer);
+
+let text_inside = hello_sayer.innerText + " from Apna College Students";
+console.log (text_inside);
+
+// Q: Create 3 divs with common class name "box". Access them & add unique text to each of them
+
+// Way 1
+let box = document.getElementsByClassName("box");
+console.log(box[0].innerText + " is of Niggas");
+console.log(box[1].innerText + " is of Brainrot");
+console.log(box[2].innerText + " is of Sigma");
+
+// Way 2:
+let boxes = document.querySelectorAll(".box"), idx = 0, genz = ['Niggas', "Brainrot", "Sigma"];
+
+for (box of boxes){
+    console.log (`${box.innerText} is of ${genz[idx]}`);
+    idx++;
+}
