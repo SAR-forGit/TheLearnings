3c08de2302435174ee4804eccb52bdf351678c6e
SAR-forGit <forwebtoons@protonmail.com>
2025-02-10 18:42:58 +0000

    12:12 AM Tue, 11 Feb 2025

--- JavaScript Experience/Day_3_with_JS_ApnaCollege(2).js
+++ JavaScript Experience/Day_3_with_JS_ApnaCollege(2).js
@@ -37,3 +37,69 @@ Fortuner.setBrand("fortuner");
 
 let Lexus = new ToyotaCar();
 Lexus.setBrand("Sub brand")
+
+// Constructor:
+class GeneralMotors {
+    constructor(brand, bodyType){
+        console.log("From General Motors");
+
+        this.brandName = brand;
+        this.Body = bodyType;
+    }
+
+}
+
+let Tahoe = new GeneralMotors("Chevrolet", "SUV");
+console.log(`About Tahoe: \n`, Tahoe);
+
+let Celestiq = new GeneralMotors("Cadillac", "Sedan");
+console.log("About Cadillac Celestiq: \n", Celestiq);
+
+
+// Inheritance
+
+class Person{
+    eat(){
+        console.log("Eating Nom Nom");
+    }
+
+    sleep(){
+        console.log("Sleeping zzzzz");
+    }
+}
+
+class Engineer extends Person{
+    develop(){
+        console.log("Github and chill");
+    }
+}
+
+let SAR = new Engineer();
+console.log(SAR.develop())
+console.log(SAR.eat())
+console.log(SAR.sleep())
+
+
+// super();
+
+class HomoSepion{
+    constructor(){
+        console.log("hi there this Parent constructor");
+    }
+}
+
+class Doc extends HomoSepion{
+    constructor(branch){
+        super(); // To invoke parent class
+        this.branch = branch;
+        console.log("This is child constructor");
+        console.log('doc is from branch:', branch);
+    }
+
+    Surgery() {
+        console.log("This is child class");
+    }
+
+}
+
+let Shuja = new Doc("Plastic Surgeon")
