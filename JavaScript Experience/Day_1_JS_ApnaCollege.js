e338b3ceb55369f8429ee086fb0b60dafdf1ba26
SAR-forGit <sar.for.official@gmail.com>
2026-08-01 19:22:11 +0530

    Implement username recommendation and validation
    Added username recommendation feature and improved user prompts for username selection.

--- JavaScript Experience/Day_1_JS_ApnaCollege.js
+++ JavaScript Experience/Day_1_JS_ApnaCollege.js
@@ -22,6 +22,38 @@ while(1){
     }
 }
 console.log(`Name = ${givenName}\nUser Name = ${userName}`);
+// Q: Improved Version:
+givenName = prompt("Enter your name: ");
+
+recomendedUsername  = "@" + givenName.toLowerCase() + givenName.length;
+
+while(1){
+    var userName = prompt(`we recomend you username: ${recomendedUsername} . Do you want to use it?`);
+
+    var userAnswer = userName;
+
+    if(userAnswer == "yes" || userAnswer == "YES" || userAnswer == "Y" || userAnswer == "y"){
+        userName = recomendedUsername;
+        break;
+    }
+    else if(userAnswer == "No" || userAnswer == "no" || userAnswer == "NO" || userAnswer == "n" || userAnswer == "N"){
+        userName = prompt("Enter your desired username: ");
+        userAnswer = prompt(`are you sure for the username ${"@"+userName}`);
+        while(1 || userAnswer == "No" || userAnswer == "no" || userAnswer == "NO" || userAnswer == "n" || userAnswer == "N"){
+            userName = prompt("Enter your desired username: ");
+            userAnswer = prompt(`are you sure for the username ${"@"+userName}`);
+            if(userAnswer == "yes" || userAnswer == "YES" || userAnswer == "Y" || userAnswer == "y") break;
+            else continue;    
+        }
+        break;
+    }
+    else{
+        userName = prompt(`Wrong Answer! Try Again.`);
+        continue;
+    }
+}
+console.log(`Hey ${givenName}, your username is set to ${userName}`);
+
 
 // Q: For a given array with marks of students -> [85, 97, 44, 37, 76, 60], Find the average marks of the entire class
 
