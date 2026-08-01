4cc89e9fac8393a7f34306cad35e77ae062da318
SAR-forGit <sar.for.official@gmail.com>
2026-08-01 19:25:57 +0530

    Add username verification script in Python
    This script prompts the user to verify or choose a username based on their input and given name.

---
+++ C++ vs JS vs Python/PythonSide.py
@@ -0,0 +1,25 @@
+# Verify your Username:
+givenName = input("Enter your name: ")
+
+recommendedUsername = "@" + givenName.lower() + str(len(givenName));
+
+while(1):
+    userName = input(f"we recommend you username {recommendedUsername}. Do you wnat to use it?")
+    userAnswer = userName
+    
+    if(userAnswer == "yes" or userAnswer == "YES" or userAnswer == "Y" or userAnswer == "y"):
+        userName = recommendedUsername
+        break
+    elif(userAnswer == "No" or userAnswer == "no" or userAnswer == "NO" or userAnswer == "n" or userAnswer == "N"):
+        userName = input("Enter your desired username: ")
+        userAnswer = input(f"are you sure for the username {"@"+userName}")
+        while(1 or userAnswer == "No" or userAnswer == "no" or userAnswer == "NO" or userAnswer == "n" or userAnswer == "N"):
+            userName = input("Enter your desired username: ");
+            userAnswer = input(f"are you sure for the username ${"@"+userName}")
+            if(userAnswer == "yes" or userAnswer == "YES" or userAnswer == "Y" or userAnswer == "y"): break
+            else: continue
+        break
+    else:
+        userName = input("Wrong Answer! Try Again.")
+        continue
+print(f"Hey ${givenName}, your username is set to {userName}")
